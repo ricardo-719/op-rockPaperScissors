fb44bd98b5e8ee50c192ed68a5df4ef438f363dd
ricardo-719 <teacherrickyricardo@gmail.com>
2022-12-23 10:58:04 +0700

    Update script with game conditions

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 
-let getComputerChoice = Math.floor(Math.random(3));
 
-console.log(getComputerChoice);
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3)
+    if (choice === 0) {
+      return 'rock'
+    } else if (choice === 1) {
+      return 'paper'
+    } else {
+      return 'scissors'
+    }
+  }
+
+  function settleMatch(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return "You lose! Paper beats Rock."
+        } else {
+            return "You win! Rock beats Scissors."
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return "You lose! Scissors beats Paper."
+        } else {
+            return "You win! Paper beats Rock."
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return "You lose! Rock beats Scissors."
+        } else {
+            return "You win! Scissors beats Paper."
+        }
+    }
+}
+
+function game(){
+    for(let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Pick your play!");
+        console.log(settleMatch(playerSelection, getComputerChoice()));
+    }
+}
+
+game();
